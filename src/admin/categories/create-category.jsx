96de0232018c24207846eab78b81d9ace4f7670c
matmaxgraphics <gbadamosimateen@gmail.com>
2024-01-28 19:44:21 +0100

    added function for creating categories

--- src/admin/categories/create-category.jsx
+++ src/admin/categories/create-category.jsx
@@ -1,14 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import PanelMainLayout from "../../layout/PanelMainLayout";
 import usePostRequest from "../../components/HttpRequests/usePostRequest";
 import axios from "axios";
 
-
 const adminToken = localStorage.getItem("adminToken");
 
-
 const CreateCategory = function () {
+  const [categoryTitle, setCategoryTitle] = useState("");
+
+  const navigateTo = useNavigate();
+
+  const formData = () => {
+    return {
+      name: categoryTitle,
+    };
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const categoryData = formData();
+
+    try {
+      const result = await usePostRequest(
+        "posts/categories",
+        categoryData,
+        adminToken
+      );
+
+      navigateTo("/admin-panel/manage-category")
+      console.log("Category created successfully: ", result);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   return (
     <PanelMainLayout>
@@ -19,22 +45,24 @@ const CreateCategory = function () {
         </Link>
       </div>
       <div className="content">
-      <form>
-        <div>
-          <label>Category Name</label>
-          <input type="text" name="title" className="input-field" />
-        </div>
-        <div>
-          <label>Description (Optional)</label>
-          <input type="text" name="description" className="input-field" />
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Category Name</label>
+            <input
+              type="text"
+              name="title"
+              className="input-field"
+              value={categoryTitle}
+              onChange={(e) => setCategoryTitle(e.target.value)}
+            />
+          </div>
 
-        <div className="btn-wrap">
-          <button type="submit" className="btn">
-            Add Category
-          </button>
-        </div>
-      </form>
+          <div className="btn-wrap">
+            <button type="submit" className="btn">
+              Add Category
+            </button>
+          </div>
+        </form>
       </div>
     </PanelMainLayout>
   );
